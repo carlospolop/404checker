ae99eb40169a22bd72c4d7633a553095f74186ff
Carlos Polop <carlospolop@gmail.com>
2024-05-04 15:30:39 +0200

    logs

--- 404checker.py
+++ 404checker.py
@@ -74,7 +74,7 @@ def requests_page_titles(response):
         for tag in soup.find_all(prob_tag):
             for bad_text in BAD_TEXTS:
                 if bad_text in tag.get_text().lower():
-                    logging.info("      [-] Bad text found: {}".format(bad_text))
+                    logging.info("      [-] Bad text found for url {}: {}".format(response.ur, bad_text))
                     return True
 
 
